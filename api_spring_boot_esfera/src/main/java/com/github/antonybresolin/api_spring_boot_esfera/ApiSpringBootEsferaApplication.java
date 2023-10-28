6b144252f873b3ee4c6aaa7862fc64f847675ceb
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2023-10-28 20:26:13 -0300

    Initial commit

---
+++ api_spring_boot_esfera/src/main/java/com/github/antonybresolin/api_spring_boot_esfera/ApiSpringBootEsferaApplication.java
@@ -0,0 +1,13 @@
+package com.github.antonybresolin.api_spring_boot_esfera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringBootEsferaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringBootEsferaApplication.class, args);
+	}
+
+}
