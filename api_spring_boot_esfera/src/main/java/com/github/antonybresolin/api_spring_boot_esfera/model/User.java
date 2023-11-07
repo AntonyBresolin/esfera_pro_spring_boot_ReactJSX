6f3f5b2d9b1496a3ba18d33b29595a79b2f42f54
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2023-11-07 17:09:34 -0300

    User Login (#14)
    Sistema de login e senha, sem criptografia, e sem hash
    Sistema de busca por nome do user por username
    Sistema de criação de user

---
+++ api_spring_boot_esfera/src/main/java/com/github/antonybresolin/api_spring_boot_esfera/model/User.java
@@ -0,0 +1,60 @@
+package com.github.antonybresolin.api_spring_boot_esfera.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false, length = 12)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 12)
+    private String password;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    public User() {
+    }
+    public User(String username, String password, String name) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
