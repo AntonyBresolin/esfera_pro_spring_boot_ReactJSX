ae534731603663519f1f267abaa499b414c2152f
Lucas Dreveck <lucasdrevc@outlook.com>
2023-11-09 16:51:24 -0300

    Add multiple restore and delete functionality to Trashcan page

--- Java-CRUD-Front/src/Pages/TrashcanPage/Trashcan.jsx
+++ Java-CRUD-Front/src/Pages/TrashcanPage/Trashcan.jsx
@@ -68,6 +68,27 @@ export const Trashcan = () => {
         setClients(newClients)
     }
 
+    const handleMultipleRestoreClient = () => {
+        if (selectedClients.length === 0){
+            setMessage(`Você não selecionou nenhum cliente.`)
+            setFunctions(() => () => {setOpenDialog(false)})
+            setOpen(true)
+        } else {
+            setMessage(`Você esta prestes a restaurar ${selectedClients.length} clientes.`)
+            setFunctions(() => multipleRestoreClient)
+            setOpen(true)
+        }
+    }
+
+    const multipleRestoreClient = () => {
+        selectedClients.map((id) => {
+            const client = Clients.filter((c) => c.clientData.id === id)
+            updateStatusClient(id, client[0])
+        })
+        const newClients = Clients.filter((c) => !selectedClients.includes(c.clientData.id))
+        setClients(newClients)
+    }
+
     //Função de deletar cliente
     const handleDeleteClient = (client) => {
         setClient(client)
@@ -81,31 +102,59 @@ export const Trashcan = () => {
         setClients(newClients)
     }
 
+    const handleMultipleDeleteClient = () => {
+        if (selectedClients.length === 0){
+            setMessage(`Você não selecionou nenhum cliente.`)
+            setFunctions(() => () => {setOpenDialog(false)})
+            setOpen(true)
+        } else {
+            setMessage(`Você esta prestes a deletar permanentemente ${selectedClients.length} clientes.`)
+            setFunctions(() => multipleDeleteClient)
+            setOpen(true)
+        }
+    }
+
+    const multipleDeleteClient = async () => {
+        selectedClients.map(async (id) => {
+            await clientDelete(id)
+        })
+        const newClients = Clients.filter((c) => !selectedClients.includes(c.clientData.id))
+        setClients(newClients)
+    }
+
     return ( 
         <div className="flex flex-row w-full font-body">
             <Menu />
             <div className="w-full h-full pb-44">
                 <div className="border-y grid grid-cols-12  items-center p-3 pl-6 bg-gray-100 text-gray-600">
-                <div className="flex items-center gap-8 text-lg col-span-1">
-                    <input type="checkbox" onClick={checkAllHandler}/>
-                </div>
-                <div className="flex items-center text-lg col-span-3">
-                    Nome
-                </div>
-                <div className="col-span-2">
-                    Endereço
-                </div>
-                <div className="col-span-3">
-                    Contato
-                </div>
-                <div className="col-span-2">
-                    CPF/CNPJ
-                </div>
+                    <div className="flex items-center gap-8 text-lg col-span-1">
+                        <input type="checkbox" onClick={checkAllHandler} className="w-4 h-4"/>
+                    </div>
+                    <div className="flex items-center text-lg col-span-3">
+                        Nome
+                    </div>
+                    <div className="col-span-2">
+                        Endereço
+                    </div>
+                    <div className="col-span-3">
+                        Contato
+                    </div>
+                    <div className="col-span-2">
+                        CPF/CNPJ
+                    </div>
+                    <div className="flex justify-evenly">
+                        <div title="Restaurar clientes selecionados" onClick={handleMultipleRestoreClient} className="rounded-full bg-gray-200 p-2 cursor-pointer hover:text-amber hover:bg-purple-contrast hover:scale-110 transition ease-in-out duration-200" >
+                                <ReloadIcon className="h-4 w-4 block" />
+                        </div>
+                        <div title="Deletar clientes selecionados" onClick={handleMultipleDeleteClient} className="rounded-full bg-gray-200 p-2 cursor-pointer hover:text-amber hover:bg-purple-contrast hover:scale-110 transition ease-in-out duration-200" >
+                                <TrashIcon className="h-4 w-4 block" />
+                        </div>                            
+                    </div>
                 </div>
                 {Clients.map((eachClient, index) => (
                     <div onClick={() => {handleDetailsClient(eachClient)}} key={index} className="border-y grid grid-cols-12 items-center p-3 pl-6">
                         <label onClick={(e) => e.stopPropagation()} className="flex items-center text-lg col-span-1">
-                            <input type="checkbox" checked={ selectedClients.includes( eachClient.clientData.id ) } value={eachClient.clientData.id} onChange={checkboxHandler}/>
+                            <input type="checkbox" checked={ selectedClients.includes( eachClient.clientData.id ) } value={eachClient.clientData.id} onChange={checkboxHandler} className="w-4 h-4"/>
                         </label>
                         <div className="flex items-center text-lg col-span-3">
                             {eachClient.clientData.name}
